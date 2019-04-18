ee2db5cd71b9da2c236f280abecf4b5f5084b166
KelbyVP <kelbyvp@gmail.com>
2019-04-18 02:25:58 -0700

    Base classes created, pre-ability system

---
+++ Source/Quest/Quest.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Quest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Quest, "Quest" );
+
+DEFINE_LOG_CATEGORY(LogQuest)
+ 
---
+++ Source/Quest/QuestPlayerController.cpp
@@ -0,0 +1,112 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "QuestPlayerController.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "Runtime/Engine/Classes/Components/DecalComponent.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "QuestCharacter.h"
+#include "Engine/World.h"
+
+AQuestPlayerController::AQuestPlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
+
+void AQuestPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	// keep updating the destination every tick while desired
+	if (bMoveToMouseCursor)
+	{
+		MoveToMouseCursor();
+	}
+}
+
+void AQuestPlayerController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("SetDestination", IE_Pressed, this, &AQuestPlayerController::OnSetDestinationPressed);
+	InputComponent->BindAction("SetDestination", IE_Released, this, &AQuestPlayerController::OnSetDestinationReleased);
+
+	// support touch devices 
+	InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AQuestPlayerController::MoveToTouchLocation);
+	InputComponent->BindTouch(EInputEvent::IE_Repeat, this, &AQuestPlayerController::MoveToTouchLocation);
+
+	InputComponent->BindAction("ResetVR", IE_Pressed, this, &AQuestPlayerController::OnResetVR);
+}
+
+void AQuestPlayerController::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AQuestPlayerController::MoveToMouseCursor()
+{
+	if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
+	{
+		if (AQuestCharacter* MyPawn = Cast<AQuestCharacter>(GetPawn()))
+		{
+			if (MyPawn->GetCursorToWorld())
+			{
+				UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, MyPawn->GetCursorToWorld()->GetComponentLocation());
+			}
+		}
+	}
+	else
+	{
+		// Trace to see what is under the mouse cursor
+		FHitResult Hit;
+		GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+
+		if (Hit.bBlockingHit)
+		{
+			// We hit something, move there
+			SetNewMoveDestination(Hit.ImpactPoint);
+		}
+	}
+}
+
+void AQuestPlayerController::MoveToTouchLocation(const ETouchIndex::Type FingerIndex, const FVector Location)
+{
+	FVector2D ScreenSpaceLocation(Location);
+
+	// Trace to see what is under the touch location
+	FHitResult HitResult;
+	GetHitResultAtScreenPosition(ScreenSpaceLocation, CurrentClickTraceChannel, true, HitResult);
+	if (HitResult.bBlockingHit)
+	{
+		// We hit something, move there
+		SetNewMoveDestination(HitResult.ImpactPoint);
+	}
+}
+
+void AQuestPlayerController::SetNewMoveDestination(const FVector DestLocation)
+{
+	APawn* const MyPawn = GetPawn();
+	if (MyPawn)
+	{
+		float const Distance = FVector::Dist(DestLocation, MyPawn->GetActorLocation());
+
+		// We need to issue move command only if far enough in order for walk animation to play correctly
+		if ((Distance > 120.0f))
+		{
+			UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, DestLocation);
+		}
+	}
+}
+
+void AQuestPlayerController::OnSetDestinationPressed()
+{
+	// set flag to keep updating destination until released
+	bMoveToMouseCursor = true;
+}
+
+void AQuestPlayerController::OnSetDestinationReleased()
+{
+	// clear flag to indicate we should stop updating the destination
+	bMoveToMouseCursor = false;
+}

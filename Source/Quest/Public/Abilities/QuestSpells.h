26b7753e56fc367c465fd4268632fd8a73884d3b
KelbyVP <kelbyvp@gmail.com>
2019-07-14 18:37:07 -0700

    Created spell data assets

---
+++ Source/Quest/Public/Abilities/QuestSpells.h
@@ -0,0 +1,35 @@
+// Copyright 2019 Kelby Van Patten, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "QuestSpells.generated.h"
+
+class UGameplayAbility;
+
+UENUM(BlueprintType)
+enum class ESpellType : uint8
+{
+	IT_Paladin UMETA(DisplayName = "Paladin"),
+	IT_Priest UMETA(DisplayName = "Priest"),
+	IT_Wizard UMETA(DisplayName = "Wizard")
+};
+
+ /** This base class holds an inventory of all available spells for a given spell type */
+ /** Since this is a base class, do not blueprint this class directly */
+UCLASS()
+class QUEST_API UQuestSpells : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	/** The type of spell -- Wizard, Priest, or Paladin */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Spell)
+		ESpellType SpellTypeEnum;
+
+	/** Array of spells */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Item)
+		TArray<TSubclassOf<class UGameplayAbility>> Spells;
+};

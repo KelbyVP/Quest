7826d8c3f3e35188b8492cfe282b5ee3ad97fd77
KelbyVP <kelbyvp@gmail.com>
2019-09-25 00:53:22 -0700

    Added Word of Power spells, but seemed to have lost Blessed ability.

--- Source/Quest/Private/Abilities/QuestGATargetActorsInRange.cpp
+++ Source/Quest/Private/Abilities/QuestGATargetActorsInRange.cpp
@@ -41,9 +41,9 @@ void AQuestGATargetActorsInRange::ConfirmTargetingAndContinue()
 		{
 			if (auto Character = Cast<APawn>(o.GetActor()))
 			{
-				CharactersInRange.AddUnique(Character);
-				UE_LOG(LogTemp, Warning, TEXT("QuestGATargetActorsInRange::ConfirmTargetingAndContinue: adding %s to target list!"),
-					*Character->GetName());
+					CharactersInRange.AddUnique(Character);
+					UE_LOG(LogTemp, Warning, TEXT("QuestGATargetActorsInRange::ConfirmTargetingAndContinue: trying to add %s to target list!"),
+						*Character->GetName());
 			}
 		}
 	}
